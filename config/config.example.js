fb0a8330613a6720949a50b4fc9d39d75db3a41c
dansky20 <dan4allu93@gmail.com>
2017-11-02 07:23:05 -0700

    Added the mongodb url to environment variables (#2)

--- config/config.example.js
+++ config/config.example.js
@@ -1,6 +1,6 @@
 // Copy this file as config.js in the same folder, with the proper database connection URI.
 
 module.exports = {
-  db: 'mongodb://username:password@url:port/db',
-  db_dev: 'mongodb://url:port/db',
+  db: process.env.MONGODB_URL_DB,
+  db_dev: process.env.MONGODB_URL_DB_DEV,
 };
